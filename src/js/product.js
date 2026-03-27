9ad9b38821214edb21236ac8fcce7d91d6816e77
Baron T. <tshibasubaron@yahoo.fr>
2026-03-27 21:57:33 +0100

    Update product.js

--- src/js/product.js
+++ src/js/product.js
@@ -7,6 +7,5 @@ loadHeaderFooter();
 
 const dataSource = new ExternalServices("tents");
 const productId = getParam("product");
-
 const product = new ProductDetails(productId, dataSource);
 product.init();
