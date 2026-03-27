ba1f6d0b38f634ac63578c17b585ddf2f2be608c
Kelsey-Hansel <kelseylhansel@gmail.com>
2026-03-27 17:48:58 -0500

    Indivdual task for week 3 code, discount on product listing pages.

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,14 +1,28 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
-            <a href="../product_pages/?product=${product.Id}">
+    const discount = product.SuggestedRetailPrice - product.FinalPrice;
+    const discountRounded = discount.toFixed(2);
+    if (discountRounded > 0) {
+        return `<li class="product-card">
+                <a href="product_pages/?product=${product.Id}">
+                <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+                <h2 class="card__brand">${product.Brand.Name}</h2>
+                <h3 class="card__name">${product.NameWithoutBrand}</h3>
+                <p class="product-card__price">$${product.FinalPrice}</p>
+                <p class="product-card__discount">Has a discount of $${discountRounded}!</p>
+                </a>
+            </li>`;
+    } else {
+        return `<li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
             <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
             <h2 class="card__brand">${product.Brand.Name}</h2>
             <h3 class="card__name">${product.NameWithoutBrand}</h3>
             <p class="product-card__price">$${product.FinalPrice}</p>
             </a>
-        </li>`;
+            </li>`;
+    }
 }
 
 export default class ProductList {
