c3be6ed695e653f8e1343cdaf8f9492a0795bcc7
nrb2002 <tshibasubaron@yahoo.fr>
2026-04-03 19:25:40 +0100

    Update product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,13 +1,13 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 
-loadHeaderFooter();
 
-const params = new URLSearchParams(window.location.search);
-const category = params.get("category") || "tents";
+const category = getParam("category") || "tents";
 const dataSource = new ExternalServices(category); //create an isntance of the ProductData (ExternalServices class)
 const element = document.querySelector(".product-list"); //select the element to display the product in
 const productsByCategory = new ProductList(category, dataSource, element); //Create an instance of the product list
+
+loadHeaderFooter();
 
 productsByCategory.init(); //call the init method to display products matching the selected category. 
