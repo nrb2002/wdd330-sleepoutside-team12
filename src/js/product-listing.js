e402acfbcd468508dbccc473c1fe5aa62a4e96d0
nrb2002 <tshibasubaron@yahoo.fr>
2026-04-03 18:32:34 +0100

    Update product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -6,9 +6,8 @@ loadHeaderFooter();
 
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category") || "tents";
+const dataSource = new ExternalServices(category); //create an isntance of the ProductData (ExternalServices class)
+const element = document.querySelector(".product-list"); //select the element to display the product in
+const productsByCategory = new ProductList(category, dataSource, element); //Create an instance of the product list
 
-const dataSource = new ExternalServices(category);
-const element = document.querySelector(".product-list");
-const productList = new ProductList(category, dataSource, element);
-
-productList.init();
+productsByCategory.init(); //call the init method to display products matching the selected category. 
